bad083b3579d7c3eed98779fd970c54628c3a415
Balkirat <cheemaballu@gmail.com>
2020-04-08 13:20:52 -0400

    styled mobile menu page

--- src/components/MobileNav/mobileNav.js
+++ src/components/MobileNav/mobileNav.js
@@ -5,54 +5,56 @@ import { Link, animateScroll as scroll } from "react-scroll";
 const mobileNav = props => {
   return (
     <nav className="mobile-nav">
-      <Link
-        activeClass="active"
-        className="mobile-nav--link"
-        style={{ textDecoration: "none" }}
-        to="about"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        About
-      </Link>
-      <Link
-        activeClass="active"
-        className="mobile-nav--link"
-        style={{ textDecoration: "none" }}
-        to="projects"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        Projects
-      </Link>
-      <Link
-        activeClass="active"
-        className="mobile-nav--link"
-        style={{ textDecoration: "none" }}
-        to="skill"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        Skills
-      </Link>
-      <Link
-        activeClass="active"
-        className="mobile-nav--link"
-        style={{ textDecoration: "none" }}
-        to="contact"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        Contact Me
-      </Link>
+      <div className="mobile-nav__container">
+        <Link
+          activeClass="active"
+          className="mobile-nav--link"
+          style={{ textDecoration: "none" }}
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          className="mobile-nav--link"
+          style={{ textDecoration: "none" }}
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          Projects
+        </Link>
+        <Link
+          activeClass="active"
+          className="mobile-nav--link"
+          style={{ textDecoration: "none" }}
+          to="skill"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          Skills
+        </Link>
+        <Link
+          activeClass="active"
+          className="mobile-nav--link"
+          style={{ textDecoration: "none" }}
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          Contact Me
+        </Link>
+      </div>
     </nav>
   );
 };
