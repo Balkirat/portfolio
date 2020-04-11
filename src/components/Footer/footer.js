76f5e36a44f69516d4c5a7597b7ec9c2836378b7
Balkirat <cheemaballu@gmail.com>
2020-04-11 00:04:09 -0400

    added mailto link and resume link

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./footer.scss";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -12,12 +12,40 @@ function footer() {
         <h1 className="footer__heading">CONTACT ME</h1>
         <div className="footer__container">
           <div className="footer__icons">
-        <a><FontAwesomeIcon className="footer__icons--border" icon={faEnvelope} /></a>
-        <a target="_blank" style={{ textDecoration: "none" }}  href="http://www.linkedin.com/in/balkirat-singh" ><FontAwesomeIcon className="footer__icons--border" icon={faLinkedinIn} /></a>
-        <a target="_blank" style={{ textDecoration: "none" }} href="https://github.com/Balkirat"><FontAwesomeIcon className="footer__icons--border" icon={faGithub} /></a>
-        <a><FontAwesomeIcon className="footer__icons--border" icon={faFile} /></a>
+            <a href="mailto:cheemaballu@gmail.com" target="_blank">
+              <FontAwesomeIcon
+                className="footer__icons--border"
+                icon={faEnvelope}
+              />
+            </a>
+            <a
+              target="_blank"
+              style={{ textDecoration: "none" }}
+              href="http://www.linkedin.com/in/balkirat-singh"
+            >
+              <FontAwesomeIcon
+                className="footer__icons--border"
+                icon={faLinkedinIn}
+              />
+            </a>
+            <a
+              target="_blank"
+              style={{ textDecoration: "none" }}
+              href="https://github.com/Balkirat"
+            >
+              <FontAwesomeIcon
+                className="footer__icons--border"
+                icon={faGithub}
+              />
+            </a>
+            <a target="_blank" href="https://drive.google.com/file/d/1_Ev5H2qC54tQr_Ig9ezoMNSQ7e0lmaGf/view?usp=sharing">
+              <FontAwesomeIcon
+                className="footer__icons--border"
+                icon={faFile}
+              />
+            </a>
             <h2>Made with ❤️ in React</h2>
-          </div> 
+          </div>
           <div className="footer__details">
             <h1>Balkirat Singh</h1>
             <h3>Email: cheemaballu@gmail.com</h3>
