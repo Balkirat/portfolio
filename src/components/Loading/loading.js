c9f42ec94d8e855b4c8c9fa5fd99da3792aaeddd
Balkirat <cheemaballu@gmail.com>
2020-05-28 13:33:15 -0400

    loading page fixed

--- src/components/Loading/loading.js
+++ src/components/Loading/loading.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactTypingEffect from "react-typing-effect";
 import "./loading.scss";
-import { motion } from "framer-motion";
+
 
 class Loading extends Component {
   constructor() {
@@ -18,33 +18,21 @@ class Loading extends Component {
   }
 
   render() {
-    const variants = {
-      open: { y: 0 },
-      closed: { x: "5%", transition: { delay: 100 } },
-    };
     return (
       <>
         {this.state.isLoaded && (
-          <motion.nav
-            className="mobile-nav"
-            variants={variants}
-            initial="open"
-            animate={this.state.isLoaded ? "open" : "closed"}
-            transition={{ damping: 900 }}
-          >
-            <div className="loading">
-              <div className="loading__text">
-                <ReactTypingEffect
-                  className="typed-cursor"
-                  speed={100}
-                  staticText=""
-                  eraseDelay={1000}
-                  typingDelay={750}
-                  text={["WELCOME TO MY PORTFOLIO"]}
-                />
-              </div>
+          <div className="loading">
+            <div className="loading__text">
+              <ReactTypingEffect
+                className="typed-cursor"
+                speed={100}
+                staticText=""
+                eraseDelay={1000}
+                typingDelay={750}
+                text={["WELCOME TO MY PORTFOLIO"]}
+              />
             </div>
-          </motion.nav>
+          </div>
         )}
       </>
     );
